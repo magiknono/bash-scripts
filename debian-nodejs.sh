aa79685703eca29cdf614b7856434a0842051057
magiknono <arno.cormier@gmail.com>
2017-11-15 18:01:29 +0100

    update setup for LTS nodejs 8 or 9 experimental

--- debian-nodejs.sh
+++ debian-nodejs.sh
@@ -21,7 +21,7 @@
 
 # LTS OR EXPERIMENTAL
 echo -e "\033[32mwelcome to nodejs quick install for debian\033[0m"
-echo -e "\033[32mEnter 6 for LTS install or 7 for EXPERIMENTAL install, followed by [ENTER]\033[0m :"
+echo -e "\033[32mEnter 8 for LTS install or 9 for EXPERIMENTAL install, followed by [ENTER]\033[0m :"
 read nodejs_setup
 
 #check_debian
@@ -29,10 +29,10 @@ read nodejs_setup
  sudo apt-get install curl
 
 #download selected nodejs script
-if [ "$nodejs_setup" == "7" ];then
-sudo curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+if [ "$nodejs_setup" == "8" ];then
+sudo curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 else 
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
 fi
 
 #install nodejs
